ae8fc0672746d962deb5b3c3030f2870c81d7f5e
lyb5382 <lyb538202@gmail.com>
2025-08-06 15:50:39 +0900

    l

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ('./css/Aboutme.css')
+
+const Aboutme = () => {
+  return (
+    <section className='about'>
+      <div className='t-wrap'>
+        <h1>Aboutme title</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, facilis?</p>
+      </div>
+    </section>
+  )
+}
+
+export default Aboutme
